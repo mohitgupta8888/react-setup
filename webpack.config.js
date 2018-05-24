ac09389fe2c5080248c733f36bd24cdc45d1fbc3
Mohit Gupta <mohitgupta8888@gmail.com>
2018-05-24 06:40:28 +0530

    Config Updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
+  mode: "development",
   entry: "./index",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    publicPath: "/assets/"
+    filename: "bundle.js"
   },
   module: {
-    // configuration regarding modules
-
     rules: [
       {
-        test: /\.jsx?$/,
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
         loader: "babel-loader",
         options: {
           presets: ["es2015"]
         },
       }
     ]
-  }
-}
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, "template/index.html")
+  })]
+};
